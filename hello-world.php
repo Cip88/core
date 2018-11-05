81a4ddb775727e4f4f25b04aac19e5de5ea0172d
Cip88 <44681730+Cip88@users.noreply.github.com>
2018-11-05 21:01:44 +0200

    Add files via upload

---
+++ hello-world.php
@@ -0,0 +1,3 @@
+<?php
+echo "hello world, how are you today".PHP_EOL;
+?>
---
+++ hello.php
@@ -0,0 +1,10 @@
+
+<<?php
+  echo "Introduceti numele: ";
+  $nume = readline();
+  echo "Hello, $nume ".PHP_EOL;
+
+
+
+
+ ?>
